db8372c1a7cc01b9de78cbe82d7ca42a1a36b3c5
We the People <69933074+Humble-Servant@users.noreply.github.com>
2023-07-04 11:38:20 -0400

    Connected front and back end

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        date_avail = request.form["date_avail"]
+        occupation = request.form["occupation"]
+        # print(f"{last_name}, {first_name}  {email}")
+        # print(f"Date available: {date_avail}")
+        # print(f"Occupation: {occupation}")
     return render_template("index.html")
 
 app.run(debug=True, port=5001)
