22b027e3bf93dade2c9fb26e7780132673a6153f
We the People <69933074+Humble-Servant@users.noreply.github.com>
2023-07-04 17:45:36 -0400

    Added email through flask

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from datetime import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
+import os
+from flask_mail import Mail, Message
+
+
+PASSWORD = os.getenv('PASSWORD')
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "r!GrH9X9$4_@AwgQUTqafA3qSG_t-m9NjA6r_Qn-Gk_GWdWswBFQB!8VE7CAg@NC"
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+app.config['MAIL_SERVER'] = "smtp.gmail.com"
+app.config['MAIL_PORT'] = 587
+# app.config['MAIL_PORT'] = 465  # For use with SSL
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = "darrellswarner@gmail.com"
+app.config['MAIL_DEFAULT_SENDER'] = "darrellswarner@gmail.com"
+app.config['MAIL_PASSWORD'] = PASSWORD
+
 db = SQLAlchemy(app)
+mail = Mail(app)
 
 
 class Form(db.Model):
@@ -24,14 +39,24 @@ def index():
         first_name = request.form["first_name"]
         last_name = request.form["last_name"]
         email = request.form["email"]
-        date_avail = datetime.strptime(request.form["date_avail"], "%Y-%m-%d")
+        date = request.form["date_avail"]
+        date_avail = datetime.strptime(date, "%Y-%m-%d")
         occupation = request.form["occupation"]
         
         form = Form(first_name=first_name, last_name=last_name, email=email, date_avail=date_avail,
                     occupation=occupation)
         db.session.add(form)
         db.session.commit()
-
+        flash(f"{first_name}, your form was submitted successfully!", "success")
+        message_body = f"Thank you for your submission, {first_name}.\n\n" \
+                       f"Here is what you submitted:\n" \
+                       f"{last_name}, {first_name}\n{email}\nDate available: {date}\nOccupation: {occupation}\n\n" \
+                       f"Thank you again!  We will be in touch shortly.\n\nVery Respectfully,\nHR"
+        message = Message(subject="Form Submission",
+                          recipients=[email],
+                          body=message_body)
+        mail.send(message)
+        
     return render_template("index.html")
 
 
